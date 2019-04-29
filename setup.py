75f20d80055997d0f21591c9c40c918630b87b12
Jingyi Zhang <jz139@duke.edu>
2019-04-29 17:21:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
       name = "LDApackage",
       version = "1.0",
+      author='Xinghong Tang, Jingyi Zhang',
+      url='https://github.com/rebeccazjy425/663_LDA_Tang-Zhang.git',
       py_modules = ['LDApackage'],
-      install_requires=[]
+      install_requires=['codecs','jieba','gensim','nltk','nltk.stem','nltk.stem.porter','GibbsSamplingDMM']
       )
